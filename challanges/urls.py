81e24e0f45ee5cbb66b77c363e09ea8dde38b5e6
bryanmsombra27 <bryanjm96@gmail.com>
2025-06-24 12:17:36 -0600

    adding dynamic param

--- challanges/urls.py
+++ challanges/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     # path('january', views.index, name='index'),
     # path('february', views.february, name='february'),
-    path('<month>', views.monthly_challange, name='monthly_challange'),
+    # path('<month>', views.monthly_challange, name='monthly_challange'),
+    path('<int:month>', views.monthly_chanllange_by_number,
+         name='monthly_challange_by_number'),
+    path('<str:month>', views.monthly_challange, name='monthly_challange'),
 ]
--- challanges/views.py
+++ challanges/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+monthly_challanges = {
+    "january": "In January, learn Python",
+    "february": "In February, learn Django",
+    "march": "In March, learn JavaScript",
+    "april": "In April, learn HTML",
+    "may": "In May, learn CSS",
+    "june": "In June, learn ",
+    "july": "In July, learn ",
+    "august": "In August, learn ",
+    "september": "In September, learn ",
+    "october": "In October, learn ",
+    "november": "In November, learn ",
+    "december": "In December, learn ",
+}
 
 
 def index(request):
@@ -16,16 +31,25 @@ def march(request):
     return HttpResponse("learm django")
 
 
-def monthly_challange(request, month):
-    challange_text = None
+def monthly_chanllange_by_number(request, month):
+    months = list(monthly_challanges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("This month is not valid")
 
-    if month == "january":
-        challange_text = "In January, learn Python"
-    elif month == "february":
-        challange_text = "In February, learn Django"
-    elif month == "march":
-        challange_text = "In March, learn JavaScript"
-    else:
-        return HttpResponseNotFound("Page not found")
+    redirect_month = months[month - 1]
+
+# USANDO EL PARAMETRO NAME DE LA FUNCION PATH, USAMOS EL ARGUMENTO ARGS PARA QUE CON LA FUNCIUON REVERSE PUEDA GENERAR AUTOMATICAMENTE EL URL DE REDIRECCION SIN NOSOTROS TENER QUE CONSTRUIRLO MANUALMENTE REEMPLAZANDO LA RUTA '/challanges/' + redirect_month y generandola automaticamente
+    redirect_path = reverse("monthly_challange", args=[redirect_month])
+
+    # return HttpResponseRedirect('/challanges/' + redirect_month)
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challange(request, month):
+    try:
+        challange_text = monthly_challanges[month]
+
+    except:
+        return HttpResponseNotFound("This month is not valid")
 
     return HttpResponse(challange_text)
