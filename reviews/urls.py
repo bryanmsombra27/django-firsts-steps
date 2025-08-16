fd8934865118d105d51514c52c2d3f30e947f528
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-16 12:35:59 -0600

    working with views

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you)
+    path("thank-you", views.ThankYouView.as_view()),
+    path("all", views.ReviewsListView.as_view()),
+    path("<int:pk>", views.SingleReviewCiew.as_view())
+    # path("thank-you", views.thank_you)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,31 +3,44 @@ from django.http import HttpResponseRedirect
 from django.views import View
 from .forms import ReviewForm
 from .models import Review
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
 # Create your views here.
 
 # clase based views IMPLEMENTATION
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
+# class ReviewView(FormView):
+class ReviewView(CreateView):
+    template_name = "reviews/review.html"
+    success_url = "/reviews/thank-you"
+    form_class = ReviewForm
+    model = Review
+    # fields = ["user_name", "review_text", "rating"]
 
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
+    # def form_valid(self, form):
+    #     form.save()
+    #     return super().form_valid(form)
+    # def get(self, request):
+    #     form = ReviewForm()
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        # review = Review.objects.get(pk=1)
-        # form = ReviewForm(request.POST, instance=review)
+    #     return render(request, "reviews/review.html", {
+    #         "form": form
+    #     })
 
-        if form.is_valid():
-            # form = form.cleaned_data
+    # def post(self, request):
+    #     form = ReviewForm(request.POST)
+    #     # review = Review.objects.get(pk=1)
+    #     # form = ReviewForm(request.POST, instance=review)
 
-            # como ya es un formulario basado en un modelo al utilizar el metodo save automaticamente se guardaran lso valores en la base de datos, con la implementacion anterior esto no era posible
-            form.save()
+    #     if form.is_valid():
+    #         # form = form.cleaned_data
 
-            return HttpResponseRedirect("/reviews/thank-you")
+    #         # como ya es un formulario basado en un modelo al utilizar el metodo save automaticamente se guardaran lso valores en la base de datos, con la implementacion anterior esto no era posible
+    #         form.save()
+
+    #         return HttpResponseRedirect("/reviews/thank-you")
 
 
 # def review(request):
@@ -53,8 +66,32 @@ class ReviewView(View):
 
 
 # generic implementation
-def thank_you(request):
-    return render(request, "reviews/thank_you.html", {
-        "has_error": False,
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html", {
+#         "has_error": False,
 
-    })
+#     })
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = "koso"
+        return context
+
+
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=1)
+        return data
+
+
+class SingleReviewCiew(DetailView):
+    template_name = "reviews/single_review.html"
+    model = Review
