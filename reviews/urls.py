3478dc88d5806f10c8bd4ed3c82ec9b4e269b305
bryanmsombra27 <bryanjm96@gmail.com>
2025-08-17 11:39:22 -0600

    working with sessions

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("all", views.ReviewsListView.as_view()),
+    path("favorite", views.AddFavoriteView.as_view()),
     path("<int:pk>", views.SingleReviewCiew.as_view())
     # path("thank-you", views.thank_you)
 ]
